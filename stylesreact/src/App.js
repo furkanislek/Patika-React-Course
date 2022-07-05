a3b17e8d3b66d8e91f53608fc8b3027d30bb91a5
Furkan Akif ISLEK <furkanakif2702@gmail.com>
2022-07-05 16:14:23 +0300

    first lesson

---
+++ stylesreact/src/App.js
@@ -0,0 +1,29 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+
+        <div
+          style={{ color: "red", backgroundColor: "#515151", paddingTop: 20 }}
+        >
+          Nulla vulputate fringilla augue at ultrices. Nulla sit amet libero
+          elementum, sodales lectus nec, varius nunc. Ut molestie ac metus et
+          suscipit. Phasellus a lorem sed sem maximus aliquam. Nam eget sagittis
+          erat, non lobortis risus. Duis varius eros eget est laoreet elementum.
+          Praesent aliquet nibh sit amet suscipit tempus.
+        </div>
+
+        <div className="alert alert-primary mt-2" role="alert">
+          A simple primary alert—check it out!
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
