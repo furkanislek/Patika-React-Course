338dc08682c5883c9f7f4871ba70ef4ae87625d8
Furkan Akif ISLEK <furkanakifislek@gmail.com>
2022-07-17 17:21:47 +0300

    usememo

--- memoization/src/App.js
+++ memoization/src/App.js
@@ -6,12 +6,11 @@ import Header from './components/Header';
 function App() {
 
   const [number, setNumber] = useState(0);
+  const [text, setText] = useState("");
 
   const data = useMemo(() => {
-    return {
-      name: 'FURKAN', number
-    };
-  }, [number]);
+    return calculateObject(number);
+  }, [number])
 
   return (
     <div className="App">
@@ -19,9 +18,18 @@ function App() {
       <hr/> 
       <h1>{number}</h1>,
       <button onClick={() => setNumber(number+1)} >click</button>
-      
+      <br/><br/>
+      <input value={text} onChange={({target}) => setText(target.value) }/>
     </div>
   );
+}
+
+function calculateObject(number){
+  console.log("Calculating...");
+  for(var i=0; i<100000; i++){}
+  console.log("Calculating Completed!");
+
+  return {name: "Furkan", number}
 }
 
 export default App;
