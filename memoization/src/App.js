7255b53803b2352e52a8bded7b98bb0532d74c04
Furkan Akif ISLEK <furkanakifislek@gmail.com>
2022-07-17 17:38:02 +0300

    useCallback

--- memoization/src/App.js
+++ memoization/src/App.js
@@ -1,35 +1,32 @@
-import './App.css';
-import {useState, useMemo} from 'react'
-import Header from './components/Header';
-
+import "./App.css";
+import { useState, useMemo, useCallback } from "react";
+import Header from "./components/Header";
 
 function App() {
-
   const [number, setNumber] = useState(0);
   const [text, setText] = useState("");
 
-  const data = useMemo(() => {
-    return calculateObject(number);
-  }, [number])
+  const increment = useCallback(() => {
+    setNumber((prevState) => prevState+1)
+  }, []);
 
   return (
     <div className="App">
-      <Header number={number<5 ? null : number} data={data}/>
-      <hr/> 
-      <h1>{number}</h1>,
-      <button onClick={() => setNumber(number+1)} >click</button>
-      <br/><br/>
-      <input value={text} onChange={({target}) => setText(target.value) }/>
+
+      <Header increment = {increment} />
+
+      <hr />
+
+      <h1>{number}</h1>
+
+
+      <br />
+      <br />
+
+      <input value={text} onChange={({ target }) => setText(target.value)} />
+
     </div>
   );
-}
-
-function calculateObject(number){
-  console.log("Calculating...");
-  for(var i=0; i<100000; i++){}
-  console.log("Calculating Completed!");
-
-  return {name: "Furkan", number}
 }
 
 export default App;
--- memoization/src/components/Header.js
+++ memoization/src/components/Header.js
@@ -1,13 +1,15 @@
 import React from "react";
 
-function Header({number,data}) {
+function Header({ number, increment }) {
   console.log("Header Component Re-rendered");
-  return <div>
-    Header - {number}
-    <br/>
-    <br/>
-    <code>{JSON.stringify(data)}</code>
-  </div>;
+  return (
+    <div>
+      Header - {number}
+      <br />
+      <br />
+      <button onClick={increment}>click</button>
+    </div>
+  );
 }
 
 export default React.memo(Header);
