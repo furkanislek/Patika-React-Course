8e2c867e415e5856a625863bae447ab68a0a8449
Furkan Akif ISLEK <furkanakifislek@gmail.com>
2022-07-17 17:09:49 +0300

    usememo first commit

--- memoization/src/App.js
+++ memoization/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {useState} from 'react'
+import {useState, useMemo} from 'react'
 import Header from './components/Header';
 
 
@@ -7,9 +7,15 @@ function App() {
 
   const [number, setNumber] = useState(0);
 
+  const data = useMemo(() => {
+    return {
+      name: 'FURKAN', number
+    };
+  }, [number]);
+
   return (
     <div className="App">
-      <Header number={number<5 ? null : number}/>
+      <Header number={number<5 ? null : number} data={data}/>
       <hr/> 
       <h1>{number}</h1>,
       <button onClick={() => setNumber(number+1)} >click</button>
