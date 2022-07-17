1946a8339fc04dfef67d91d9cacb7935f4b57fdd
Furkan Akif ISLEK <furkanakifislek@gmail.com>
2022-07-17 16:59:55 +0300

    memoization

--- memoization/src/components/Header.js
+++ memoization/src/components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Header({number}) {
   console.log("Header Component Re-rendered");
-  return <div>Header  {number}</div>;
+  return <div>Header - {number}</div>;
 }
 
 export default React.memo(Header);
