d2ca5af4d18f12122d303aff39f7f1a9db54c2cf
Furkan Akif ISLEK <furkanakifislek@gmail.com>
2022-08-09 11:44:33 +0300

    Login and Logout Buttons

--- context-api/src/components/Profile.js
+++ context-api/src/components/Profile.js
@@ -1,13 +1,41 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import UserContext from "../context/UserContext";
 
 function Profile() {
-  const {user} = useContext(UserContext);
+  const { user, setUser } = useContext(UserContext);
+  const [loading, setLoading] = useState(false);
 
+  const handleLogin = () => {
+    setLoading(true);
+    setTimeout(() => {
+      setUser({ id: 1, username: "furukderki", bio: "developer" });
+      setLoading(false);
+    }, 1500);
+  };
 
-  return <div>
-    {JSON.stringify(user)}
-  </div>;
+  const handleLogout = () => {
+    setUser(null)
+  }
+
+  return (
+    <div>
+      {!user && (
+        <button onClick={handleLogin}>
+          {loading ? "Loading ...." : "Login"}
+        </button>
+      )}
+
+      <br />
+      {JSON.stringify(user)}
+
+      <br/>
+      <br/>
+
+      {
+        user && <button onClick={handleLogout}>Logout</button>
+      }
+    </div>
+  );
 }
 
 export default Profile;
